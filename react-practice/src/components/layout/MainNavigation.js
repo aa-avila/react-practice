e6f8fdc6707504c9b95b0ba3135f2ec5b27f8dd4
Agustin <agualvila@gmail.com>
2021-12-23 11:45:15 -0300

    header customization

--- react-practice/src/components/layout/MainNavigation.js
+++ react-practice/src/components/layout/MainNavigation.js
@@ -1,9 +1,11 @@
 import { Link } from 'react-router-dom';
 
+import classes from './MainNavigation.module.css';
+
 function MainNavigation() {
   return (
-    <header>
-      <div>My super app</div>
+    <header className={classes.header}>
+      <div className={classes.appName}>My super app</div>
       <nav>
         <ul>
           <li>
