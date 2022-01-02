57e7eb2c50b5209e4a748eea9befee3729134d9b
Agustin <agualvila@gmail.com>
2022-01-02 16:07:27 -0300

    add dynamic and validation food text

--- react-practice/src/components/food/FoodList.js
+++ react-practice/src/components/food/FoodList.js
@@ -1,3 +1,4 @@
+import validate from 'validate.js';
 import { useContext } from 'react';
 import FoodContext from '../../store/food-context';
 
@@ -7,17 +8,50 @@ function FoodList() {
   const foodCtx = useContext(FoodContext);
 
   const { food1, food2, food3 } = foodCtx;
+
+  const FoodText = () => {
+    const foodItems = [food1, food2, food3];
+    let validatedFoodItems = [];
+
+    foodItems.forEach((element) => {
+      if (!validate.isEmpty(element)) {
+        validatedFoodItems.push(element);
+      }
+    });
+
+    let htmlText;
+
+    //no items
+    if (validatedFoodItems.length === 0) {
+      htmlText = `No tengo ninguna comida favorita porque soy un amargo. Bye!`;
+    }
+
+    //1 item
+    if (validatedFoodItems.length === 1) {
+      htmlText = `La única comida ques me gusta es ${validatedFoodItems[0]}.`;
+    }
+    //2 items
+    if (validatedFoodItems.length === 2) {
+      htmlText = `La comida que más me gusta es ${validatedFoodItems[0]}. Despúes le
+          sigue ${validatedFoodItems[1]}.`;
+    }
+
+    //3 items
+    if (validatedFoodItems.length === 3) {
+      htmlText = `La comida que más me gusta es ${validatedFoodItems[0]}. Despúes le
+          sigue ${validatedFoodItems[1]}. Y por por último, en el tercer lugar se encuentra ${validatedFoodItems[2]}.`;
+    }
+
+    return (
+      <Typography variant="body1" component="div" sx={{ mb: 1 }}>
+        {htmlText}
+      </Typography>
+    );
+  };
+
   return (
     <Box sx={{ p: 2, width: '45%', boxShadow: 2, bgcolor: '#fff' }}>
-      <Typography variant="body1" component="div" sx={{ mb: 1 }}>
-        La comida que más me gusta es {food1}.
-      </Typography>
-      <Typography variant="body1" component="div" sx={{ mb: 1 }}>
-        En segundo lugar le sigue {food2}.
-      </Typography>
-      <Typography variant="body1" component="div" sx={{ mb: 1 }}>
-        Por último, en el tercer lugar se encuentra {food3}.
-      </Typography>
+      <FoodText />
     </Box>
   );
 }
